4ac45ca6aa7f92fafbd630ffc4083c8513490496
sunxin <sunxin000@outlook.com>
2022-04-26 16:51:09 +0800

    given propensity analysis

--- propensity_analysis.py
+++ propensity_analysis.py
@@ -1,17 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+# train_matrix = np.loadtxt(f"./data/coat/train.ascii", dtype=int)
+# train_propensity = np.loadtxt(f"./data/coat/propensities.ascii", dtype=float)
 
-train_matrix = np.loadtxt(f"./data/coat/train.ascii", dtype=int)
-train_propensity = np.loadtxt(f"./data/coat/propensities.ascii", dtype=float)
-
-user, item = np.where(train_matrix)
-target = train_matrix[user, item]
-propensity = train_propensity[user, item]
-
+# user, item = np.where(train_matrix)
+# target = train_matrix[user, item]
+# propensity = train_propensity[user, item]
+# torch.save(propensity, 'data/propensity/coat_origin.pt')
+# np.savetxt('data/propensity/coat_origin.txt', propensity)
+path = 'data/propensity/raw/coat_epoch_10_-1.pt'
+propensity = torch.load(path)
 plt.hist(propensity, bins=20)
 
-plt.title("Propensity Analyze")
+plt.title(path)
 plt.xlabel('Propensity')
 plt.ylabel('Amount')
 
-plt.savefig('propensity_analyze.jpg')
+plt.savefig(f'{path}.jpg')
